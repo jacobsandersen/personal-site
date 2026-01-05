a5f7914f190741e94e07df2b7ca6847a7008540b
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-01-05 09:32:21 +0800

    Get rid of opennext and cloudflare since it doesn't support next 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,6 @@
-import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
 export default nextConfig
-
-initOpenNextCloudflareForDev()
