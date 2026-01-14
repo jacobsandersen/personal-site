686508a8f02a6d39446095a380c80f3f0f0131c8
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-01-14 19:28:33 +0800

    fix(typo): mp2->mf2

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,10 +1,10 @@
 import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
-import { Mp2DocumentSchema } from "./types/mp2-document";
+import { Mf2DocumentSchema } from "./types/mf2-document";
 
 const scribble = defineCollection({
     loader: glob({ pattern: "**/*.json", base: "./src/content/scribble" }),
-    schema: Mp2DocumentSchema
+    schema: Mf2DocumentSchema
 })
 
 export const collections = { scribble };
--- src/types/mf2-document.ts
+++ src/types/mf2-document.ts
@@ -1,8 +1,8 @@
 import { z } from "astro:content"
 
-export const Mp2DocumentSchema = z.object({
+export const Mf2DocumentSchema = z.object({
     type: z.array(z.string()),
     properties: z.record(z.array(z.unknown()))
 })
 
-export type Mp2Document = z.infer<typeof Mp2DocumentSchema>
+export type Mf2Document = z.infer<typeof Mf2DocumentSchema>
