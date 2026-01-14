e93be6de38e2fd8a76d12a8085807fa79574e682
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-01-14 08:41:53 +0800

    Update import path for types

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,6 +1,6 @@
 import { glob } from "astro/loaders";
 import { defineCollection } from "astro:content";
-import { Mp2DocumentSchema } from "./src/types/mp2-document";
+import { Mp2DocumentSchema } from "./types/mp2-document";
 
 const scribble = defineCollection({
     loader: glob({ pattern: "**/*.json", base: "./src/content/scribble" }),
