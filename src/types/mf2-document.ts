4944860e70c2ee418e9cbb0d7154a955114f7af5
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-01-24 18:32:09 +0800

    Get started with rendering

--- src/types/mf2-document.ts
+++ src/types/mf2-document.ts
@@ -1,8 +1,12 @@
 import { z } from "astro:content"
 
+export const Mf2PropertiesSchema = z.record(z.array(z.unknown()))
+
+export type Mf2Properties = z.infer<typeof Mf2PropertiesSchema>
+
 export const Mf2DocumentSchema = z.object({
     type: z.array(z.string()),
-    properties: z.record(z.array(z.unknown()))
+    properties: Mf2PropertiesSchema
 })
 
 export type Mf2Document = z.infer<typeof Mf2DocumentSchema>
