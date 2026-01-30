90e01f5b9107b4e99f50c992616229286faf0e6a
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-01-30 22:17:26 +0800

    Change page slug strategy, get started with fleshing out routes

--- src/util/url.ts
+++ src/util/url.ts
@@ -6,3 +6,19 @@ export function isValidUrl(urlString: string): boolean {
         return false;
     }
 }
+
+export function validateYearForPath(year?: string): boolean {
+    return !!year && /^\d{4}$/.test(year);
+}
+
+export function validateMonthForPath(month?: string): boolean {
+    return !!month && /^(0[1-9]|1[0-2])$/.test(month);
+}
+
+export function validateDayForPath(day?: string): boolean {
+    return !!day &&/^(0[1-9]|[12]\d|3[01])$/.test(day);
+}
+
+export function validateYearMonthDayForPath(year?: string, month?: string, day?: string): boolean {
+    return validateYearForPath(year) && validateMonthForPath(month) && validateDayForPath(day)
+}
