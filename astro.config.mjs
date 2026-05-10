b79335b01b820a1960c9ae0320f8b0f251937fb2
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-05-10 19:11:15 +0800

    Add site option to config so that the sitemap generates

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://jacobandersen.dev",
+
   vite: {
     plugins: [tailwindcss()],
   },
