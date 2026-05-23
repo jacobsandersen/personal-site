d229b67db17b62b0acff9968cc015d445d30b362
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-05-23 23:00:46 +0800

    Finish up initial reading pages with previous/wanted

--- src/lib/seer.ts
+++ src/lib/seer.ts
@@ -1,8 +1,25 @@
 import { env } from "cloudflare:workers"
+import NowReading from "~/components/ssr/NowReading.astro"
 
 interface SeerResp<T> {
   message: string,
   data: T | undefined
+}
+
+export interface HardcoverData {
+  pagination: {
+    current_page: number,
+    total_pages: number
+  },
+  books: HardcoverBook[]
+}
+
+export interface HardcoverBook {
+  title: string,
+  author: string,
+  image: string,
+  last_read: string,
+  times_read: number
 }
 
 export interface NowReading {
@@ -17,8 +34,24 @@ export interface NowPlaying {
   }
 }
 
+export async function getPreviousReads(page: number): Promise<HardcoverData | undefined> {
+  return request(`hardcover/books?status=read&limit=10&page=${page}`)
+}
+
+export async function getWantToRead(page: number): Promise<HardcoverData | undefined> {
+  return request(`hardcover/books?status=wanted&limit=10&page=${page}`)
+}
+
 export async function getNowReading(): Promise<NowReading | undefined> {
-  return request("hardcover/now")
+  const data = await request<HardcoverData>("hardcover/books?status=current&limit=1")
+  if (!data || !data.books) {
+    return undefined
+  }
+
+  return {
+    title: data.books[0].title,
+    author: data.books[0].author
+  }
 }
 
 export async function getNowPlaying(): Promise<NowPlaying | undefined> {
@@ -28,6 +61,7 @@ export async function getNowPlaying(): Promise<NowPlaying | undefined> {
 async function request<T>(path: string): Promise<T | undefined> {
   const res = await env.SEER.fetch(`https://internal/${path}`)
   if (!res.ok) {
+    console.log(res)
     return undefined
   }
 
