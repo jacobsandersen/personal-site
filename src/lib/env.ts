eaf2425ecfc7645c46ce060e9a2f5c3b5178b244
Jacob Andersen <19645494+jacobsandersen@users.noreply.github.com>
2026-05-27 01:10:27 +0800

    hopefully final

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -4,10 +4,10 @@ import { z } from 'zod';
 const envSchema = z.object({
   SEER_URL: z.url(),
   SEER_FIXED_AUTH: z.string(),
-  TELEMETRY_ENABLE: z.boolean().default(false),
+  TELEMETRY_ENABLE: z.coerce.boolean().default(false),
   TELEMETRY_OTEL_EXPORTER_ENDPOINT: z.url().optional(),
   HOST: z.string().default('0.0.0.0'),
-  PORT: z.number().default(4321),
+  PORT: z.coerce.number().default(4321),
   NODE_ENV: z.enum(['development', 'production']).default('production'),
 });
 
